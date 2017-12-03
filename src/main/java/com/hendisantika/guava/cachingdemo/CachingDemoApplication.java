7220cdf5994a0f152feae4780d2a230c8d5742f7
Hendi Santika <hendisantika@gmail.com>
2017-12-03 08:11:01 +0700

    #5 - Enable Caching

--- src/main/java/com/hendisantika/guava/cachingdemo/CachingDemoApplication.java
+++ src/main/java/com/hendisantika/guava/cachingdemo/CachingDemoApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.guava.cachingdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CachingDemoApplication {
 
 	public static void main(String[] args) {
