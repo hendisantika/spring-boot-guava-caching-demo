e8e9c3c0e2acd897f9d4d80049fb74ba66f63bc5
Hendi Santika <hendisantika@gmail.com>
2017-12-03 08:05:44 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/guava/cachingdemo/CachingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.guava.cachingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingDemoApplication.class, args);
+	}
+}
