7d053676eff4e31b7f449fafa61a1466da4bcfdf
Hendi Santika <hendisantika@gmail.com>
2017-12-03 08:18:18 +0700

    #10 - Add Share Enum

---
+++ src/main/java/com/hendisantika/guava/cachingdemo/entity/Shares.java
@@ -0,0 +1,15 @@
+package com.hendisantika.guava.cachingdemo.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : caching-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/12/17
+ * Time: 08.17
+ * To change this template use File | Settings | File Templates.
+ */
+public enum  Shares {
+    AKZO, SHELL, ASML, UNILEVER, ING;
+}
